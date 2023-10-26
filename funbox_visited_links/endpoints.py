4c2f284c61792e2b2d5461db678e0bea79fc6a9a
lereto <leretto91@gmail.com>
2023-10-26 23:49:50 +0300

    connected redis via DI

---
+++ funbox_visited_links/endpoints.py
@@ -0,0 +1,51 @@
+import re
+from datetime import datetime
+from typing import Annotated
+
+from dependency_injector.wiring import inject, Provide
+from fastapi import Query, Depends, APIRouter
+from pydantic import BaseModel, constr
+
+from .containers import Container
+from .services import Service
+
+router = APIRouter()
+
+
+class LinksValidator(BaseModel):
+    _regexp: str = r"(?:https?:\/\/)?([a-zA-Z0-9.-]+\.[a-z]+)"
+    links: list[constr(pattern=_regexp)]
+
+    def extract_domains(self):
+        return {
+            re.match(self._regexp, link).group(1)
+            for link in self.links
+        }
+
+
+@router.post("/visited_links")
+@inject
+def visited_links(
+        json_data: LinksValidator,
+        service: Service = Depends(Provide[Container.redis_service])
+):
+    unix_timestamp = int(datetime.timestamp(datetime.now()))
+    domains = json_data.extract_domains()
+    service.set(domains, unix_timestamp)
+
+    return {"status": "ok"}
+
+
+@router.get("/visited_domains")
+@inject
+def visited_domains(
+        _from: Annotated[int, Query(alias="from")],
+        to: int,
+        service: Service = Depends(Provide[Container.redis_service])
+):
+    domains = service.get(_from, to)
+
+    return {
+        "domains": domains,
+        "status": "ok"
+    }
